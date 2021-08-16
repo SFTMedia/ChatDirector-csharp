2075e98b6b6ffc08803e84da7875e25ce1c409b3
blalp <26471229+blalp@users.noreply.github.com>
2021-08-15 20:06:34 -0700

    update

--- rust/src/modules/rust/RustModule.cs
+++ rust/src/modules/rust/RustModule.cs
@@ -2,10 +2,29 @@ using System;
 using System.Collections.Generic;
 using ChatDirector.core;
 
+// Use them so they get imported.
+/*
+using YamlDotNet;
+using M2Mqtt;
+*/
+
 namespace Oxide.Ext.RustChatDirector
 {
     public class RustModule : IModule
     {
+        /*
+        public RustModule() {
+            var _useless_yaml = new YamlDotNet.Serialization.Deserializer();
+            var _useless_mqtt = new M2Mqtt.MqttProtocolVersion();
+            // Do some pointless operation to avoid getting ommitted.
+            if (_useless_mqtt.GetType() == _useless_yaml.GetType()) {
+                Console.WriteLine("ERROR IMPORT Shouldn't be possible");
+            }
+            ChatDirector.core.ChatDirector.addModule(new ChatDirector.core.ConsoleModule());
+            ChatDirector.core.ChatDirector.addModule(new ChatDirector.extra.MQTTModule());
+            ChatDirector.core.ChatDirector.addModule(new ChatDirector.extra.StateModule());
+        }
+        */
         public List<string> getItemNames()
         {
             string[] temp = { "rust-input", "rust-output" };
